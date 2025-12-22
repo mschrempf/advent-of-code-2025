179c68e14b291a8ba6aebdb1f1edfab8f594db01
Matthias Schrempf <mail@makarl.de>
2025-12-22 08:28:21 +0100

    Day 1 Part 1

---
+++ src/bin/day01.rs
@@ -0,0 +1,42 @@
+use std::io::Read;
+
+fn parse_input(input: &str) -> Vec<i32> {
+    input.lines().map(|line| {
+        let line = line.trim();
+        let factor = if line.starts_with("L") { -1} else {1};
+        let line = line.trim_matches('L').trim_matches('R');
+        let amount: i32 = line.parse().expect("cannot parse input");
+        factor * amount
+    }
+    ).collect()
+}
+
+fn dial(rotations: &[i32]) -> Vec<i32> {
+    let mut values = vec![50];
+
+    for &r in rotations {
+        values.push((values.last().expect("at least one element exists in values") + r).rem_euclid(100));
+    }
+
+    values
+}
+
+fn part1(input: &[i32]) -> usize {
+    dial(input).iter().filter(|&&n| n == 0).count()
+}
+
+
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).expect("Reading input must work");
+    let parsed_input = parse_input(&input);
+    println!("Part 1: {}", part1(&parsed_input));
+}
+
+
+#[test]
+fn test_part1() {
+    let input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
+
+    assert_eq!(part1(input), 3);
+}
